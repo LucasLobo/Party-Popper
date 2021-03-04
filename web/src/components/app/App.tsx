4763ea4e2a39c20b109006abd152c23a98147fc1
LucasLobo <lucas5798fell@gmail.com>
2021-03-04 19:55:02 +0000

    Fix format

--- web/src/components/app/App.tsx
+++ web/src/components/app/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from '../../assets/logo.svg';
+import React from "react";
+import logo from "../../assets/logo.svg";
 
-import './App.css';
+import "./App.css";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
